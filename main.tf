7f250d737c78301a1d15ad5d189bc461b9377e87
devopsbyrohit09 <devopsbyrohit09@gmail.com>
2025-08-19 23:54:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 provider "aws" {
-    region = "us-east-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  ami                    = "ami-0de716d6197524dd9"
-  instance_type          = "t2.micro"
-  key_name               = "Rohitkeypair"
+ami                    = "ami-0de716d6197524dd9"
+instance_type          = "t2.micro"
   
-  tags = {
-    Name = "hcp-instance"
-  }
+tags = {
+Name = "hcp-instance"
+}
 }

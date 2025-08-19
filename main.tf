8122edd7901952b6589228d910ad40629b6bf1a6
devopsbyrohit09 <devopsbyrohit09@gmail.com>
2025-08-19 23:37:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+  ami                    = "ami-0de716d6197524dd9"
+  instance_type          = "t2.micro"
+  key_name               = "Rohitkeypair"
+  
+  tags = {
+    Name = "My_Instance"
+  }
+}
